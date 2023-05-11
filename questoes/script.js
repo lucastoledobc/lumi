65788ff68ff626854898557224fccc4fb0391a59
ToledoLBC <lucastoledobm@gmail.com>
2023-05-11 17:12:24 -0300

    script

--- questoes/script.js
+++ questoes/script.js
@@ -1,28 +1,24 @@
 function gabarito() {
     var opt = document.getElementsByName('opt');
-    //var gab = document.getElementsByClassName('gab');
     var res = document.getElementById('res');
-
-    document.getElementById("gab").style.background = "green";
 
     for (c=10;c<40;c+=10) {
         document.getElementById(c).style.display = 'block';
     }
 
-    var c1=0
     for (c=0;c<4;c++) {
-        c1 -=1
         if (opt[c].checked) {
-            document.getElementById(c1).style.background = "red";
+            document.getElementById(c+5).style.background = "red";
         }
-    } 
-    
-    gab = 2
-
-    if (opt[gab].checked) {
-        res.innerHTML = `Resposta Certa`;
     }
-    else {
+
+    var selected = document.querySelector("input[name='opt']:checked").value;
+    if (selected=='1') {
+        res.innerHTML = `Resposta Certa`;
+        let g = document.querySelector("input[name='opt']:checked").id;
+        g = Number(g);
+        document.getElementById(g+5).style.background = "green";
+    } else {
         res.innerHTML = `Resposta Errada`;
     }
 }
