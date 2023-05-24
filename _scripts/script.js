aef29507b5c2655ad7f56fbc5d1d3a17d063847e
ToledoLBC <lucastoledobm@gmail.com>
2023-05-24 20:11:48 -0300

    nav2

--- _scripts/script.js
+++ _scripts/script.js
@@ -1,12 +1,18 @@
+
 function flexivel() {
-    if (menus.style.display == 'block') {
-        menus.style.display = 'none'
+    let nav2 = document.getElementById('nav2');
+    if (nav2.style.opacity == '1') {
+        nav2.style.opacity = '0.9';
+        nav2.style.left = '-200px';
+        nav2.style.width = '200px';
     }
     else {
-        menus.style.display = 'block'
+        nav2.style.opacity = '1';
+        nav2.style.left = '0px';
+        nav2.style.width = '100vw';
+        nav2.style.transition = '0.5s';
     }
 }
-
 
 
 let caixa = document.getElementById('search');
