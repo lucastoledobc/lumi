0703c6f23486259eb78c3ec5aa1197c77fd73f1f
ToledoLBC <lucastoledobm@gmail.com>
2023-12-04 21:14:56 -0300

    game-info

--- _scripts/game.js
+++ _scripts/game.js
@@ -1,22 +1,36 @@
-const body = document.getElementsByTagName("body")[0];
-body.innerHTML = ``;
+// const body = document.getElementsByTagName("body")[0];
+// body.innerHTML += `
+
+// `;
 
 
 
 function h1() {
-    const h1 = document.getElementById("h1");
-    h1.style.display = 'none';
+    document.getElementById("hum").style.display = 'none';
 }
 
 
 
+
+// WARNING
+
 const war = document.getElementById("war");
-war.innerHTML = `
+// war.innerHTML = `
 
-`;
+// `;
 
+const img = document.getElementById("info");
 
-
-function warning() {
-    war.style.display = 'block';
+function info() {
+    if (war.style.display != 'block') {
+        war.style.display = 'block';
+        img.innerHTML = `
+        <img src="/_midias/icon-close.svg" id="iinfo" class="icon" alt="Informação"></img>
+        `;
+    } else {
+        war.style.display = 'none';
+        img.innerHTML = `
+        <img src="/_midias/icon-info.svg" id="iinfo" class="icon" alt="Informação"></img>
+        `;
+    }
 }
