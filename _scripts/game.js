06a89b28489e35962590a5db4df7e73d8af3a0db
ToledoLBC <lucastoledobm@gmail.com>
2024-06-25 20:32:17 -0300

    botão do game

--- _scripts/game.js
+++ _scripts/game.js
@@ -9,35 +9,72 @@ botoes.innerHTML = `
             <img src="/_midias/icon-info.svg" id="iinfo" class="icon" alt="Informação">
         </button>
         <div class="warn" id="warn">
-            Caso esteja tendo problemas desative o adblock, pode ajudar.
-            Para outros casos você pode me mandar uma mensagem descrevendo o problema abaixo.
-            <form action="">
-                <label for="msg">Mensagem:</label><br>
-                <input type="text" value="" placeholder="Descreva o problema aqui...">
-            </form>
         </div>
+        <button type="button" id="maxi" class="button">
+            <img src="/_midias/icon-maxi.svg" id="imaxi" class="icon" alt="Maximizar" onclick="maxi()">
+        </button>
 
 `;
 
 // WARNING
 
 const war = document.getElementById("warn");
-// war.innerHTML = `
+    war.innerHTML = `
+        Caso esteja tendo problemas desative o adblock, pode ajudar.
+            Para outros casos você pode me mandar uma mensagem descrevendo o problema abaixo.
+            <form action="">
+                <label for="msg">Mensagem:</label><br>
+                <input type="text" value="" placeholder="Descreva o problema aqui...">
+            </form>
+        `;
 
-// `;
-
-const img = document.getElementById("info");
 
 function info() {
+    let img = document.getElementById("info");
     if (warn.style.display != 'block') {
         warn.style.display = 'block';
         img.innerHTML = `
-        <img src="/_midias/icon-close.svg" id="iinfo" class="icon" alt="Informação"></img>
+        <img src="/_midias/icon-close.svg" id="iinfo" class="icon" alt="Informação">
         `;
     } else {
         warn.style.display = 'none';
         img.innerHTML = `
-        <img src="/_midias/icon-info.svg" id="iinfo" class="icon" alt="Informação"></img>
+        <img src="/_midias/icon-info.svg" id="iinfo" class="icon" alt="Informação">
         `;
     }
 }
+
+function maxi() {
+    let img = document.getElementById("maxi");
+    let elem = document.documentElement;
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen(), img.innerHTML = `
+        <img src="/_midias/icon-mini.svg" id="imaxi" class="icon" alt="Minimizar" onclick="mini()">
+        `;
+      } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen(), img.innerHTML = `
+        <img src="/_midias/icon-mini.svg" id="imaxi" class="icon" alt="Minimizar" onclick="mini()">
+        `;
+      } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen(), img.innerHTML = `
+        <img src="/_midias/icon-mini.svg" id="imaxi" class="icon" alt="Minimizar" onclick="mini()">
+        `;
+      }
+    }
+
+function mini() {
+    let img = document.getElementById("maxi");
+    if (document.exitFullscreen) {
+        document.exitFullscreen(), img.innerHTML = `
+        <img src="/_midias/icon-maxi.svg" id="imaxi" class="icon" alt="Maximizar" onclick="maxi()">
+        `;
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen(), img.innerHTML = `
+        <img src="/_midias/icon-maxi.svg" id="imaxi" class="icon" alt="Maximizar" onclick="maxi()">
+        `;
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen(), img.innerHTML = `
+        <img src="/_midias/icon-maxi.svg" id="imaxi" class="icon" alt="Maximizar" onclick="maxi()">
+        `;
+    }
+    }  
