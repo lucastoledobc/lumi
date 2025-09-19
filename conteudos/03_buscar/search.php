66594e7c90522ec5745e1aa4da42d1b8a6419eac
lucastoledobc <lucastoledobc@gmail.com>
2025-09-19 13:52:53 -0300

    def

---
+++ conteudos/03_buscar/search.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Lumi</title>
+    <link rel="stylesheet" href="../lumi/anexoslumi/estilos/style.css" media="all">
+    <script src="../lumi/anexoslumi/scripts/script.js" defer></script>
+
+</head>
+<body>
+    <header></header>
+    <main>
+        <p>Resultado da pesquisa:</p>
+        <section id="res">
+            <?php 
+            echo "oi";
+            ?>
+        </section>
+    </main>
+
+    <footer></footer>
+</body>
+</html>
